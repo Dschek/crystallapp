de9b29e9a86ff1b1b20363a681104c1aa5fd5eb1
Dmitrii Chekmazov <d.chekmazov@compassplus.com>
2022-01-22 15:10:26 +0500

    1

---
+++ src/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default param=>{
+    <div class='menu'>
+        <ul>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+            <li>crystal 1</li>
+        </ul>
+    </div>
+}
